15b335b047c3b478951897f03e0c80d74d6d9e4c
F219298 <F219298@cfd.nu.edu.pk>
2023-07-10 14:31:50 +0500

    Uncommented

--- FixedPoint.cpp
+++ FixedPoint.cpp
@@ -1,105 +1,105 @@
-//#include<iostream>
-////#include<cmath>//Fixed_Point Method
-//using namespace std;
-//
-//double intervalChecker_der(int* coeff, double a, int deg)//to find value in derivative
-//{
-//	double po, temp = deg, final = 0;
-//	for (int i = 0; i < deg; i++)
-//	{
-//		po = pow(a, temp);
-//		po = coeff[i] * po;
-//		final += po;
-//		temp--;
-//	}
-//	return final;
-//}
-//
-//double intervalChecker(int* coeff, double a, int deg)//to find value in actual function
-//{
-//	double po, temp = deg, final = 0;
-//	for (int i = 0; i < deg+1; i++)
-//	{
-//		po = pow(a, temp);
-//		po = coeff[i] * po;
-//		final += po;
-//		temp--;
-//	}
-//	return final;
-//}
-//
-//void display(int* coeff_arr, int deg)//displaying equation
-//{
-//	cout << endl << "             <--------------------------------------Equation--------------------------------->" << endl << endl;
-//	cout << "                                          ";
-//	int temp = deg;
-//	for (int i = 0; i < deg + 1; i++)
-//	{
-//		cout << "(" << coeff_arr[i] << ")" << "x^" << temp;
-//		if (i < deg)
-//		{
-//			cout << "+";
-//		}
-//		temp--;
-//	}
-//}
-//
-//int main()
-//{
-//	int* coeff_arr,accu;
-//	int deg;
-//	double a, b, fun1, fun2, avg, temp1,deri, iter;
-//	cout << "Enter Highest Degree of x:";
-//	cin >> deg;
-//	int temp = deg;
-//	coeff_arr = new int[deg + 1];
-//	for (int i = 0; i < deg + 1; i++)
-//	{
-//		cout << "Enter Coefficient of x^" << temp << ":";
-//		cin >> coeff_arr[i];
-//		temp--;
-//	}
-//	system("cls");
-//	display(coeff_arr, deg);
-//	cout << endl;
-//	do {
-//		cout << "Enter Interval value a:";
-//		cin >> a;
-//		cout << "Enter Interval value b:";
-//		cin >> b;
-//		fun1 = intervalChecker(coeff_arr, a, deg);
-//		fun2 = intervalChecker(coeff_arr, b, deg);
-//		cout << "Interval value: [" << a << "," << b << "]" << endl;
-//		cout << "f(a):" << fun1 << endl;
-//		cout << "f(b):" << fun2 << endl;
-//	} while (fun1 < 0 && fun2 < 0 || fun1>0 && fun2>0);
-//	cout << "Enter Accuracy value in terms of 10^:";
-//	cin >> accu;
-//	double x_not = (a + b) / 2;
-//	temp = deg;
-//	int *temp_arr = new int[deg + 1];//calculating derivative
-//	for (int i = 0; i < deg+1; i++)
-//	{
-//		temp_arr[i] = coeff_arr[i];
-//	}
-//	for (int i = 0; i < deg+1; i++)
-//	{
-//		temp_arr[i] = temp_arr[i] * temp;//derivative stored in temp_arr
-//		temp--;
-//	}
-//	cout << "[x0]:" << x_not << endl;//value of x_not
-//	iter = b - a;
-//	iter = (accu - log10(iter)) / log10(2);
-//	iter = iter + 1;
-//	iter = iter * -1;
-//	for (int i = 0; i<iter; i++)
-//	{
-//		deri = intervalChecker_der(temp_arr, x_not, deg-1);
-//		temp1= intervalChecker(coeff_arr, x_not, deg);
-//		temp1 = temp1 / deri;
-//		temp1 = x_not - temp1;
-//		x_not = temp1;
-//		cout <<"[x"<<i+1<<"]:" << temp1 << endl;
-//	}
-//	return 0;
-//}
+#include<iostream>
+//#include<cmath>//Fixed_Point Method
+using namespace std;
+
+double intervalChecker_der(int* coeff, double a, int deg)//to find value in derivative
+{
+	double po, temp = deg, final = 0;
+	for (int i = 0; i < deg; i++)
+	{
+		po = pow(a, temp);
+		po = coeff[i] * po;
+		final += po;
+		temp--;
+	}
+	return final;
+}
+
+double intervalChecker(int* coeff, double a, int deg)//to find value in actual function
+{
+	double po, temp = deg, final = 0;
+	for (int i = 0; i < deg+1; i++)
+	{
+		po = pow(a, temp);
+		po = coeff[i] * po;
+		final += po;
+		temp--;
+	}
+	return final;
+}
+
+void display(int* coeff_arr, int deg)//displaying equation
+{
+	cout << endl << "             <--------------------------------------Equation--------------------------------->" << endl << endl;
+	cout << "                                          ";
+	int temp = deg;
+	for (int i = 0; i < deg + 1; i++)
+	{
+		cout << "(" << coeff_arr[i] << ")" << "x^" << temp;
+		if (i < deg)
+		{
+			cout << "+";
+		}
+		temp--;
+	}
+}
+
+int main()
+{
+	int* coeff_arr,accu;
+	int deg;
+	double a, b, fun1, fun2, avg, temp1,deri, iter;
+	cout << "Enter Highest Degree of x:";
+	cin >> deg;
+	int temp = deg;
+	coeff_arr = new int[deg + 1];
+	for (int i = 0; i < deg + 1; i++)
+	{
+		cout << "Enter Coefficient of x^" << temp << ":";
+		cin >> coeff_arr[i];
+		temp--;
+	}
+	system("cls");
+	display(coeff_arr, deg);
+	cout << endl;
+	do {
+		cout << "Enter Interval value a:";
+		cin >> a;
+		cout << "Enter Interval value b:";
+		cin >> b;
+		fun1 = intervalChecker(coeff_arr, a, deg);
+		fun2 = intervalChecker(coeff_arr, b, deg);
+		cout << "Interval value: [" << a << "," << b << "]" << endl;
+		cout << "f(a):" << fun1 << endl;
+		cout << "f(b):" << fun2 << endl;
+	} while (fun1 < 0 && fun2 < 0 || fun1>0 && fun2>0);
+	cout << "Enter Accuracy value in terms of 10^:";
+	cin >> accu;
+	double x_not = (a + b) / 2;
+	temp = deg;
+	int *temp_arr = new int[deg + 1];//calculating derivative
+	for (int i = 0; i < deg+1; i++)
+	{
+		temp_arr[i] = coeff_arr[i];
+	}
+	for (int i = 0; i < deg+1; i++)
+	{
+		temp_arr[i] = temp_arr[i] * temp;//derivative stored in temp_arr
+		temp--;
+	}
+	cout << "[x0]:" << x_not << endl;//value of x_not
+	iter = b - a;
+	iter = (accu - log10(iter)) / log10(2);
+	iter = iter + 1;
+	iter = iter * -1;
+	for (int i = 0; i<iter; i++)
+	{
+		deri = intervalChecker_der(temp_arr, x_not, deg-1);
+		temp1= intervalChecker(coeff_arr, x_not, deg);
+		temp1 = temp1 / deri;
+		temp1 = x_not - temp1;
+		x_not = temp1;
+		cout <<"[x"<<i+1<<"]:" << temp1 << endl;
+	}
+	return 0;
+}
--- Secant.cpp
+++ Secant.cpp
@@ -1,95 +1,95 @@
-//#include<iostream>
-//#include<cmath>//Secant Method
-//using namespace std;
-//struct function
-//{
-//	double fun1, fun2;
-//};
-//double calculate_fx(int* coeff, double a, int deg)//to find value in actual function
-//{
-//	double po, temp = deg, final = 0;
-//	for (int i = 0; i < deg + 1; i++)
-//	{
-//		po = pow(a, temp);
-//		po = coeff[i] * po;
-//		final += po;
-//		temp--;
-//	}
-//	return final;
-//}
-//
-//void display(int* coeff_arr, int deg)//displaying equation
-//{
-//	cout << endl << "             <--------------------------------------Equation--------------------------------->" << endl << endl;
-//	cout << "                                          ";
-//	int temp = deg;
-//	for (int i = 0; i < deg + 1; i++)
-//	{
-//		cout << "(" << coeff_arr[i] << ")" << "x^" << temp;
-//		if (i < deg)
-//		{
-//			cout << "+";
-//		}
-//		temp--;
-//	}
-//}
-//
-//function intervalChecker(double a, double b, int* coeff_arr, int deg)
-//{
-//	function f;
-//	double fun1, fun2;
-//	do {
-//		a = a + 0.1;
-//		b = b - 0.1;
-//		fun1 = calculate_fx(coeff_arr, a, deg);
-//		fun2 = calculate_fx(coeff_arr, b, deg);
-//	} while (fun1 < 0 && fun2 < 0 || fun1>0 && fun2>0);
-//	f.fun1 = a;
-//	f.fun2 = b;
-//	return f;
-//}
-//
-//int main()
-//{
-//	int* coeff_arr;
-//	int deg;
-//	double a, b, fun1, fun2,iter;
-//	double x, temp1, temp2;
-//	function f;
-//	cout << "Enter Highest Degree of x:";
-//	cin >> deg;
-//	int temp = deg;
-//	coeff_arr = new int[deg + 1];
-//	for (int i = 0; i < deg + 1; i++)
-//	{
-//		cout << "Enter Coefficient of x^" << temp << ":";
-//		cin >> coeff_arr[i];
-//		temp--;
-//	}
-//	system("cls");
-//	display(coeff_arr, deg);
-//	cout << endl;
-//	do {
-//		cout << "Enter Interval value a:";
-//		cin >> a;
-//		cout << "Enter Interval value b:";
-//		cin >> b;
-//		fun1 = calculate_fx(coeff_arr, a, deg);
-//		fun2 = calculate_fx(coeff_arr, b, deg);
-//		cout << "Interval value: [" << a << "," << b << "]" << endl;
-//		cout << "f(a):" << fun1 << endl;
-//		cout << "f(b):" << fun2 << endl;
-//	} while (fun1 < 0 && fun2 < 0 || fun1>0 && fun2>0);
-//	f = intervalChecker(a, b, coeff_arr, deg);//picking x0 and x1
-//	for (int i = 0; i < 15; i++)
-//	{
-//		temp1 = calculate_fx(coeff_arr, f.fun2, deg);
-//		temp2 = calculate_fx(coeff_arr, f.fun1, deg);
-//		x = (f.fun1 * temp1) - (f.fun2 * temp2);
-//		temp1 = temp1 - temp2;
-//		x = x / temp1;
-//		cout << "x[" << i+2 << "]=" << x << endl;
-//		f.fun1 = f.fun2;
-//		f.fun2 = x;
-//	}
-//}
+#include<iostream>
+#include<cmath>//Secant Method
+using namespace std;
+struct function
+{
+	double fun1, fun2;
+};
+double calculate_fx(int* coeff, double a, int deg)//to find value in actual function
+{
+	double po, temp = deg, final = 0;
+	for (int i = 0; i < deg + 1; i++)
+	{
+		po = pow(a, temp);
+		po = coeff[i] * po;
+		final += po;
+		temp--;
+	}
+	return final;
+}
+
+void display(int* coeff_arr, int deg)//displaying equation
+{
+	cout << endl << "             <--------------------------------------Equation--------------------------------->" << endl << endl;
+	cout << "                                          ";
+	int temp = deg;
+	for (int i = 0; i < deg + 1; i++)
+	{
+		cout << "(" << coeff_arr[i] << ")" << "x^" << temp;
+		if (i < deg)
+		{
+			cout << "+";
+		}
+		temp--;
+	}
+}
+
+function intervalChecker(double a, double b, int* coeff_arr, int deg)
+{
+	function f;
+	double fun1, fun2;
+	do {
+		a = a + 0.1;
+		b = b - 0.1;
+		fun1 = calculate_fx(coeff_arr, a, deg);
+		fun2 = calculate_fx(coeff_arr, b, deg);
+	} while (fun1 < 0 && fun2 < 0 || fun1>0 && fun2>0);
+	f.fun1 = a;
+	f.fun2 = b;
+	return f;
+}
+
+int main()
+{
+	int* coeff_arr;
+	int deg;
+	double a, b, fun1, fun2,iter;
+	double x, temp1, temp2;
+	function f;
+	cout << "Enter Highest Degree of x:";
+	cin >> deg;
+	int temp = deg;
+	coeff_arr = new int[deg + 1];
+	for (int i = 0; i < deg + 1; i++)
+	{
+		cout << "Enter Coefficient of x^" << temp << ":";
+		cin >> coeff_arr[i];
+		temp--;
+	}
+	system("cls");
+	display(coeff_arr, deg);
+	cout << endl;
+	do {
+		cout << "Enter Interval value a:";
+		cin >> a;
+		cout << "Enter Interval value b:";
+		cin >> b;
+		fun1 = calculate_fx(coeff_arr, a, deg);
+		fun2 = calculate_fx(coeff_arr, b, deg);
+		cout << "Interval value: [" << a << "," << b << "]" << endl;
+		cout << "f(a):" << fun1 << endl;
+		cout << "f(b):" << fun2 << endl;
+	} while (fun1 < 0 && fun2 < 0 || fun1>0 && fun2>0);
+	f = intervalChecker(a, b, coeff_arr, deg);//picking x0 and x1
+	for (int i = 0; i < 15; i++)
+	{
+		temp1 = calculate_fx(coeff_arr, f.fun2, deg);
+		temp2 = calculate_fx(coeff_arr, f.fun1, deg);
+		x = (f.fun1 * temp1) - (f.fun2 * temp2);
+		temp1 = temp1 - temp2;
+		x = x / temp1;
+		cout << "x[" << i+2 << "]=" << x << endl;
+		f.fun1 = f.fun2;
+		f.fun2 = x;
+	}
+}
